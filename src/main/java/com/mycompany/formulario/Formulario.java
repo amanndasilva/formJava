5d81072a10f39bac65cb3a41ca23ff9bbd4ebd07
Amanda Silva <61585589+amanndasilva@users.noreply.github.com>
2024-10-23 22:25:00 -0300

    projeto
    formulário parcialmente finalizado

---
+++ src/main/java/com/mycompany/formulario/Formulario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formulario;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Formulario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
